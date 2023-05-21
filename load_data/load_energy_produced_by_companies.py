70e9ebe7f0525e1e2badc67ffcccf5d29ec36fa5
Araceli Sebastian <aracelissc2@gmail.com>
2023-05-21 18:14:46 +0200

    add clean data function for powerplants

--- load_data/load_energy_produced_by_companies.py
+++ load_data/load_energy_produced_by_companies.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import os
 pd.options.mode.chained_assignment = None
-
+from sklearn.impute import SimpleImputer
 from utils.parameters import ENERGY_COMPANIES_FOLDER, ENERGY_COMPANY_SECTOR
 
 def load_company_sectors()-> pd.DataFrame:
@@ -26,14 +26,17 @@ def load_company_data_by_sector():
     # get folder with company data
     power_plant_list = os.listdir(ENERGY_COMPANIES_FOLDER)
     power_plant_list.sort()
+    for file in power_plant_list:
+        if 'Identifier' in file:
+            power_plant_list.remove(file)
 
     for file in power_plant_list:
         #print(filename)
         # load file
         filename = os.path.join(ENERGY_COMPANIES_FOLDER, file)
-        df = pd.read_csv(filename, delimiter=',', decimal='.')
-        # TODO average data if there is more than one column
-        # append df to
+        df = pd.read_csv(filename, delimiter=';', decimal='.')
+        # clean df
+        df = clean_data(df)
         company = file.split('_')[0]
         condition = sector_df['Name'].str.contains(company)
         sector_df[condition]
@@ -42,3 +45,25 @@ def load_company_data_by_sector():
         dict_companies[company_sector].append(df)
 
     return dict_companies
+
+
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    cols =  df.columns[3:]
+    df['Date'] = pd.to_datetime(df['Date'], format='%d.%m.%Y')
+    for col in cols:
+        df[col] = df[col].str.replace('.', '')
+        df[col] = df[col].str.replace(',', '.')
+        df[col] = df[col].replace('-', np.nan)
+        df[col] = df[col].astype('float32')
+        nan_val = (df[col].isna().value_counts()*100/df.shape[0])
+        if nan_val[1] <= 30:
+            imputer = SimpleImputer(strategy="mean")
+            df[col] = imputer.fit_transform(df[[col]])
+        else:
+            df = df.drop(col)
+
+    if len(df.columns) > 4:
+        df['avg energy [MW]'] = df[cols].mean(axis=1)
+        df = df.drop(cols)
+
+    return df
